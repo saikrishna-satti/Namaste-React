c8dabde5057c5169b81b27da01a8af011e3788f7
sai krishna <skrishna.reddy.satti@gmail.com>
2024-12-07 16:39:57 +0530

    initialized react in project

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from "react";
+ import ReactDOM from "react-dom"
+const parent=React.createElement("div",{id:"parent"},[
+
+     React.createElement("div",{id:"child1"},[
+       React.createElement("h1",{},"i am h1 tag"),
+       React.createElement("h2",{},"i am h2 tag")
+    ]),
+    React.createElement("div",{id:"child1"},[
+        React.createElement("h1",{},"i am h1 tag"),
+        React.createElement("h2",{},"i am h2 tag")
+     ]),
+     React.createElement("div",{id:"child1"},[
+      React.createElement("h1",{},"i am h1 tag"),
+      React.createElement("h2",{},"i am h2 tag")
+   ]) 
+
+]);
+const heading=React.createElement("h1",{},"hello world from react");
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
